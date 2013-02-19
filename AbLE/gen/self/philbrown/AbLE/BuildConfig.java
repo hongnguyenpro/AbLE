51196a00b70c24cd48777021f586c3c8cf147593
Phil Brown <philip22brown@gmail.com>
2013-02-19 16:49:27 -0600

    added bin and gen to gitignore

--- AbLE/gen/self/philbrown/AbLE/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package self.philbrown.AbLE;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- AbLETest/gen/self/philbrown/AbLETest/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package self.philbrown.AbLETest;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
